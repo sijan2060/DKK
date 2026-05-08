094f6ebd42f53dcaa1c954b6cf37d499c374f7b5
Sijan Maharjan <sijanpr07@gmail.com>
2026-05-08 20:51:03 +0545

    Update Message =

---
+++ src/data/usersData.js
@@ -0,0 +1,16 @@
+export const usersData = [
+{
+id: 'u1',
+name: 'Admin User',
+email: 'admin@dilipdance.com',
+role: 'admin',
+password: 'password123' // Only for mock validation
+},
+{
+id: 'u2',
+name: 'Dilip Student',
+email: 'student@dilipdance.com',
+role: 'user',
+password: 'password123'
+}
+];
